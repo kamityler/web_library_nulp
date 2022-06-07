3759b1d9db76e1f401c0571fe754e73c14fcc591
Danylo <bodnar.dp@gmail.com>
2022-06-08 02:09:22 +0300

    main_css

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import {InputGroup, FormControl, Button} from 'react-bootstrap'
 import Image from '../Image/Image';
+
+import './main.css'
 
 class Main extends Component {
     render() {
         return (
             <div>
+                <InputGroup className="mb-3 search-pannel">
+                    <FormControl
+                    placeholder="Search"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                    />
+                    <Button variant="outline-secondary" id="button-addon2">
+                        Search
+                    </Button>
+                </InputGroup>
+
                 <Image url="https://www.educba.com/academy/wp-content/uploads/2020/10/CSS-Border-Radius.jpg" text="kjk"/>
                 <Image url="https://www.educba.com/academy/wp-content/uploads/2020/10/CSS-Border-Radius.jpg" text="kjk"/>
                 <Image url="https://www.educba.com/academy/wp-content/uploads/2020/10/CSS-Border-Radius.jpg" text="kjk"/>
